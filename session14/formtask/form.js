c7d9a5eaedae0d7fa7b915a24c462daae45070e0
Thiru0328 <gnanamkthiru@gmail.com>
2023-08-27 09:11:23 +0530

    newly added

---
+++ session14/formtask/form.js
@@ -0,0 +1,31 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let gender = document.getElementById("gender").value;
+    let mobileNumber = document.getElementById("mobileNumber").value;
+    let food = document.getElementById("food").value;
+    let address = document.getElementById("address").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let country = document.getElementById("country").value;
+    let pincode = document.getElementById("pincode").value;
+
+  
+    let userDetails = {
+      firstname,
+      lastname,
+      gender,
+      mobileNumber,
+      food,
+      address,
+      city,
+      state,
+      country,
+      pincode
+
+    };
+
+    console.log("User Details: ", userDetails);
+  }
